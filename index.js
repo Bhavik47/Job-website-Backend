c528c4c01b71f4025ae4a17c817c4f9045c4a462
Bhavik47 <116140035+Bhavik47@users.noreply.github.com>
2025-05-17 12:21:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://job-website-kappa.vercel.app/',
+    origin:'https://localhost:5173',
     credentials:true
 }
 
